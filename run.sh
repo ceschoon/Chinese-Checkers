b295618ba8184f858c9b1f78fdf05b61beb3ec0c
ceschoon <ceschoon@ulb.ac.be>
2020-02-04 23:41:42 +0100

    compilation script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+if [ $# -gt 0 ]
+then
+	if [ $1 == "debug" ]
+	then
+		g++ -o chinese_checkers \
+			main.cpp Algorithm.h Algorithm.cpp Board.h Board.cpp \
+			-lsfml-graphics -lsfml-window -lsfml-system \
+			-DDEBUG
+		./chinese_checkers > out.txt 2> out2.txt
+	fi
+else
+	g++ -o chinese_checkers \
+		main.cpp Algorithm.h Algorithm.cpp Board.h Board.cpp \
+		-lsfml-graphics -lsfml-window -lsfml-system
+	./chinese_checkers 
+fi
+
